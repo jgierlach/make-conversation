e5abfe1b4b5101256b88e992e0e8f5e7d9460f72
jgierlach <jegierlach@gmail.com>
2018-07-28 15:47:15 -0500

    set up basic views

---
+++ client/components/GameInstructions.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GameInstructions = () => (
+    <div className='container'>
+      <h2 className='center'>Click the button below to find out what your question is!</h2>  
+    </div>
+)
+
+module.exports = GameInstructions
